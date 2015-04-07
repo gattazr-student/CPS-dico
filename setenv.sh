45b2a134db5b17d7a58a4f4578a9d1a562a5e3cd
gattazr <remi.gattaz@gmail.com>
2015-04-07 08:26:45 +0200

    Modify setenv to work anywhere

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # need to be sourced (source setenv.sh)
-export DYLD_LIBRARY_PATH=/Users/gattazr/Documents/Cours/polytech/S6/CPS/projet/lib/libmacos64:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$(pwd)/lib/libmacos64:$DYLD_LIBRARY_PATH
+echo $DYLD_LIBRARY_PATH
