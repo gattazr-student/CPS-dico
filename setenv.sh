8903ab2e5e00f163a0c68d5f6722d058fd74fa53
gattazr <remi.gattaz@gmail.com>
2015-04-07 08:30:32 +0200

    Correction of 2 small errors on setenv and Makefil

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # need to be sourced (source setenv.sh)
 export DYLD_LIBRARY_PATH=$(pwd)/lib/libmacos64:$DYLD_LIBRARY_PATH
-echo $DYLD_LIBRARY_PATH
+echo DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH
